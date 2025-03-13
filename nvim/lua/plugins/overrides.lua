b50373b9f9476eda53e1b171519f657f9558d45d
Nathan <51066514+goosebruce@users.noreply.github.com>
2025-03-13 14:25:43 -0700

    updates main

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -63,7 +63,30 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
   },
-
+  {
+    "folke/noice.nvim",
+    ---@module 'noice'
+    opts = {
+      lsp = {
+        enabled = true,
+        -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+        -- See the section on formatting for more details on how to customize.
+        --- @type NoiceFormat|string
+        format = "lsp_progress",
+        --- @type NoiceFormat|string
+        format_done = "lsp_progress_done",
+        throttle = 1000 / 30, -- frequency to update lsp progress message
+        view = "notify",
+      },
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+  },
   {
     "folke/noice.nvim",
     ---@module 'noice'
@@ -92,10 +115,26 @@ return {
       keymap = {
         preset = "enter",
       },
+      cmdline = {
+        keymap = {
+          -- recommended, as the default keymap will only show and select the next item
+          ["<Tab>"] = { "show", "accept" },
+        },
+        completion = {
+          menu = {
+            auto_show = function(ctx)
+              return vim.fn.getcmdtype() == ":"
+              -- enable for inputs as well, with:
+              -- or vim.fn.getcmdtype() == '@'
+            end,
+          },
+        },
+      },
       completion = {
         menu = {
           max_height = 20,
           direction_priority = { "n" },
+          max_height = 20,
           border = "padded",
         },
         list = {
